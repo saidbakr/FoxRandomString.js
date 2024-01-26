5027bddf1adb58a222d191615c72d82293b20bec
Said Bakr <said.fox@gmail.com>
2024-01-26 06:41:23 +0200

    adding local demo

---
+++ demo/FoxRandomString.js
@@ -0,0 +1,157 @@
+  const FoxRandomString = new Object({
+  special_chars:'+-@!%*}[/)$#(>=~^',
+  url_safe:'-_.~.~_-',
+  numbers: '0123456789234567',
+  lowercase: 'abcdefghijklmnopqurstwvxyz',
+  uppercase: 'ABCDEFGHIJKLMNOPQURSTWVXYZ',
+  hex: '0A16B278C3D4E5F9',
+  mixPat: /^(?=.*[a-z])(?=.*[0-9])(?=.*[A-Z])(?=.*[_$@.\+\W])[a-zA-Z0-9\W]{1,}$/,
+  ulnPat: /^(?=.*[a-z])(?=.*[0-9])(?=.*[A-Z])[a-zA-Z0-9]{1,}$/,
+  type: 'mix',// mix, int, lwr, upr, uln, hex
+  length: 4,
+  
+  setLength: function(i){
+    const val = parseInt(i);
+    if (val < 4 || isNaN(val)){
+      return 4;
+    }
+    else{
+      return val;      
+    }
+  },
+  setType: function(t){
+    switch(t){
+      case 'int':
+        return 'int';
+      case 'lwr':
+        return 'lwr';
+      case 'upr':
+        return 'upr';
+      case 'uln':
+        return 'uln';
+      case 'hex':
+        return 'hex';
+      case 'urs':
+        return 'urs';
+      default:
+        return 'mix';
+    }    
+  },
+  setSelection: function(t){
+    switch(t){
+      case 'int':
+        return this.numbers;
+      case 'lwr':
+        return this.lowercase;
+      case 'upr':
+        return this.uppercase;
+      case 'uln':
+        return this.uppercase+this.lowercase+this.numbers;
+      case 'hex':
+        return this.hex;
+      case 'urs':
+        return this.uppercase+this.lowercase+this.numbers+this.url_safe;
+      default:
+        return this.lowercase+this.uppercase+this.numbers+this.special_chars;
+    }    
+  },
+  genRand: function(x){
+    return Math.floor(Math.random() * Math.floor(x));
+  },
+  testOutput: function(type,output){
+    switch (type){
+      case 'mix':
+        return this.tested(this.mixPat,output);
+      case 'uln':
+        return this.tested(this.ulnPat,output);
+      case 'urs':
+        return this.tested(this.mixPat,output);
+      default:
+        return output;
+    }    
+  },
+  tested: function(pat,input){
+    const re = new RegExp(pat);    
+    if(!input.match(re)){      
+      return this.filledSlots(input,this.type);       
+     
+    }
+    else{      
+      return input;
+    }
+  },
+  filledSlots: function(input, type){
+    if (type == 'mix' || type == 'uln' || type == 'urs'){
+      const slots = this.getSlots(input.length, type);
+      return this.fillSlots(input,slots,type)
+    }
+  },
+  fillSlots: function(input,slots,type){
+    input = input.split('')
+    if (type == 'mix'){
+      input[slots[0]] = this.numbers[this.genRand(this.numbers.length)];
+      input[slots[1]] = this.lowercase[this.genRand(this.lowercase.length)];
+      input[slots[2]] = this.uppercase[this.genRand(this.uppercase.length)];
+      input[slots[3]] = this.special_chars[this.genRand(this.special_chars.length)];
+    }
+    if (type == 'urs'){
+      input[slots[0]] = this.numbers[this.genRand(this.numbers.length)];
+      input[slots[1]] = this.lowercase[this.genRand(this.lowercase.length)];
+      input[slots[2]] = this.uppercase[this.genRand(this.uppercase.length)];
+      input[slots[3]] = this.url_safe[this.genRand(this.url_safe.length)];
+    }
+    if (type == 'uln'){
+      input[slots[0]] = this.numbers[this.genRand(this.numbers.length)];
+      input[slots[1]] = this.lowercase[this.genRand(this.lowercase.length)];
+      input[slots[2]] = this.uppercase[this.genRand(this.uppercase.length)];
+      
+    }
+    input = input.join('')
+    return input;
+  },
+  getSlots: function(length,type){
+    const output = [];
+    let slotsStr = '';
+    if (type == 'mix' || type == 'urs'){
+      for (let i = 0; i < 4; i++){
+        const randVal = this.atomicFor(slotsStr,length);        
+        output.push(randVal);
+        slotsStr += randVal;
+      }
+    }
+    else if (type == 'uln'){
+      for (let i = 0; i < 3; i++){
+        const randVal = this.atomicFor(slotsStr,length);  
+        output.push(this.genRand(length));
+        slotsStr += randVal;
+      }
+    }
+    return output;
+    
+  },
+  atomicFor: function(str,length){
+    let randVal = this.genRand(length);
+    while (!this.isAtomicIn(randVal,str)){
+           randVal = this.genRand(length);
+           }
+      return randVal;
+  },
+  isAtomicIn: function(char,str){
+    if(str.indexOf(char) > -1){
+      return false;
+    }
+    return true;
+  },
+  generate: function(length,type){
+    let output = '';    
+    this.type = this.setType(type);
+    this.length = this.setLength(length);
+    const selection = this.setSelection(this.type)
+    
+    for (let i = 0; i < this.length; i++){
+      output += selection[this.genRand(selection.length)];
+    }
+    return this.testOutput(this.type,output);
+  }
+  
+});
---
+++ demo/index.js
@@ -0,0 +1,89 @@
+//import { FoxRandomString } from "foxrandomstring";
+// The template of hex values at 6 chars length
+let colorTpl = `<span style='background:#{VAL}'><span class='highlight'>{VAL}</span></span><br />`;
+// Creating an object of the FoxRandomString
+let rand = Object.create(FoxRandomString);
+// Reserving the original class's special chars list
+let originalSpChars = rand.special_chars;
+// Getting the DOM of the regenerate button
+const genBtn = document.querySelector("#genBtn");
+// Adding a click event listener to the regenerate button
+genBtn.addEventListener("click", function () {
+  // Getting the length desired for the string from Length field
+  let l = document.querySelector("#len").value;
+  // Getting the string type from the dropdown list Type
+  let t = document.querySelector("#type").value;
+  // Loading the custom special chars
+  let special_chars = document.querySelector("#spChars").value.trim();
+  // Determine the choice of custom special chars is allowed or not
+  let isCustomSpChars = document.querySelector("#yes").checked;
+  // Sets the rand object special chars from the custom or from the original
+  rand.special_chars = isCustomSpChars ? special_chars : originalSpChars;
+  gen(l, t);
+});
+const yes = document.querySelector("#yes");
+yes.addEventListener("change", function () {
+  if (this.checked) {
+    document.querySelector("#spChars").removeAttribute("disabled");
+    this.parentNode.getElementsByTagName("span")[0].textContent = "Yes";
+  } else {
+    document.querySelector("#spChars").setAttribute("disabled", "disabled");
+    this.parentNode.getElementsByTagName("span")[0].textContent = "No";
+  }
+});
+// Validate Special Characters field to contain special characters only
+let spCharsField = document.querySelector("#spChars");
+let initVal = spCharsField.value;
+// Regex that match unrepeated non word characters
+const regex = /^(?:(\W)(?!.*\1))*$/g;
+spCharsField.addEventListener("change", function () {
+  if (!this.value.match(regex) || this.value.length < 1) {
+    this.value = initVal;
+    alert(
+      "This field should not be empty and it should contain unrepeated special characters only!"
+    );
+  } else {
+    initVal = this.value;
+  }
+});
+/*
+ * @return string
+ * @val: the string value
+ * @isColor: boolean
+ * It printout the colorTpl if isColor true
+ */
+function printVal(val, isColor) {
+  if (isColor) {
+    return colorTpl.replaceAll("{VAL}", val);
+  } else {
+    return val + "<br />";
+  }
+}
+/*
+ * @return string
+ * @l: int the length of the string
+ * @t: string the type of the string, uln, int,lwr, upr, hex, urs, mix
+ */
+function gen(l, t) {
+  // the initial output
+  let out = "";
+  // no color by default
+  let isColor = false;
+  // check if the desired type hex and the length 6 to be a color
+  if (l == 6 && t == "hex") {
+    // Yes it is a hex color format
+    isColor = true;
+  }
+  // Looping to generate 10 strings
+  for (let i = 0; i < 10; i++) {
+    // Using the rand object's method generate to make the a random string with the specified length and type
+    let val = rand.generate(l, t);
+    // Concatenates the value of out.
+    out += printVal(val, isColor);
+  }
+  // Replcing the contents of the element #app with the final output
+  document.querySelector("#app").innerHTML = out;
+}
+// Initially generate default random strings list with length 16 and type mix
+gen(16, "mix");
+ 
